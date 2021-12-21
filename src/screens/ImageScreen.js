e9e73fd389658d7de7bdee8a8741b1eb21ffbe05
Robinson, Joshua <joshua.robinson@aruplab.com>
2021-12-21 15:33:47 -0500

    Current progress

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  return (
+    <View>
+      <ImageDetail
+        title="Eagle"
+        imageSource={require("../../assets/eagle.png")}
+      />
+      <ImageDetail
+        title="Heart"
+        imageSource={require("../../assets/heart.png")}
+      />
+      <ImageDetail
+        title="Some Guy"
+        imageSource={require("../../assets/someguy.png")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
