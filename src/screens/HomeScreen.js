64121c3bb82743575c6302d0d80cdcb2b8b01f4e
Robinson, Joshua <joshua.robinson@aruplab.com>
2021-12-22 12:05:10 -0500

    Added SquareScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,6 +25,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Color")}
         title="Go to Color Screen Demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to Square Screen Demo"
+      />
     </View>
   );
 };
