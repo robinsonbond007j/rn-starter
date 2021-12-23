2f252d933fa1994803a3a12a69719c094aeb9059
Robinson, Joshua <joshua.robinson@aruplab.com>
2021-12-23 15:01:40 -0500

    Woohoo went from useState to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  //This is the way we want to use state and action
+  //state === {counter: number}
+  //action === {type: change_counter payload: + 1 || -1}
+
+  switch (action.type) {
+    case "Increase":
+      return { ...state, counter: state.counter + action.payload };
+    case "Decrease":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "Increase", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "Decrease", payload: 1 });
         }}
       />
       <Text>Current Count: {counter}</Text>
