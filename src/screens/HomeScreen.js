aaf3217a4ac676b70ba6c884114aaa8d8c1b049b
Robinson, Joshua <joshua.robinson@aruplab.com>
2021-12-27 14:23:51 -0500

    Added text screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -29,6 +29,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Square")}
         title="Go to Square Screen Demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Text")}
+        title="Go to Text Screen Demo"
+      />
     </View>
   );
 };
